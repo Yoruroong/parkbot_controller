2b2609853f84c02a396202786654eef7b12cc4ae
Yoruroong <tuntun4132@naver.com>
2020-08-20 13:50:21 +0900

    fuck

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react'
 import './App.css'
+import Table from 'react-bootstrap/Table'
 
 import {
   BrowserRouter as Router,
@@ -12,20 +13,6 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      username:null
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3001/api')
-      .then(res=>res.json())
-      .then(data=>this.setState({username:data.username}));
-  }
-
   render() {
     return (
       <Router>
@@ -40,7 +27,7 @@ class App extends React.Component {
 }
 
 function Controller() {
-  let match = useRouteMatch();
+  let match = useRouteMatch()
 
   return (
     <div>
@@ -54,7 +41,7 @@ function Controller() {
         </Route>
       </Switch>
     </div>
-  );
+  )
 }
 
 let send
@@ -63,7 +50,17 @@ function Guild() {
   fetch(`http://localhost:3001/api/${guildId}`).then(res=>res.json()).then(data=>send=data)
   console.log(send)
   if(!send) return <h1>길드정보가 없습니다. 올바른 경로로 접속했는지 확인해주세요.</h1>
-  return <p>{send.queue.split("[#").join("<br />[#")}</p>
+  let datadata = send.queue.split("\n").join("</tr><th>").split("&yoru%").join('</th><th>')
+  return (
+    <div>
+      <img src={send.thumbnail[0]}></img>
+      <p></p>
+      <h1>현재 재생 중: {send.title[0]}</h1>
+      <Table striped bordered hover>
+
+      </Table>
+    </div>
+  )
 }
 
 export default App;

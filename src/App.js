1fc1ce44bf6674a140a1ab0f1cb0f28cd2353aac
Yoruroong <tuntun4132@naver.com>
2020-08-20 14:06:56 +0900

    test

--- src/App.js
+++ src/App.js
@@ -13,54 +13,44 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
+  componentDidMount() {
+    this.renderAll()
+    let match = useRouteMatch()
+  }
+  renderAll = async() => {
+    let { guildId } = useParams()
+    try {
+      let res = await axios.get(`http://localhost:3001/api/${guildId}`);
+      let will = res.data
+      // this will re render the view with new data
+      this.setState({
+        rendered: will.title[0]
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
   render() {
     return (
       <Router>
         <div className="App">
         <Route path="/controller">
-          <Controller />
+          <div>
+            <Switch>
+              <Route path={`${match.path}/:guildId`}>
+                {this.state.rendered}
+              </Route>
+              <Route path={match.path}>
+                <h1 class="center">올바른 경로로 접속해 주세요.</h1>
+              <Link to="/">Home</Link>
+              </Route>
+            </Switch>
+          </div>
         </Route>
         </div>
       </Router>
     )
   }
-}
-
-function Controller() {
-  let match = useRouteMatch()
-
-  return (
-    <div>
-      <Switch>
-        <Route path={`${match.path}/:guildId`}>
-          <Guild />
-        </Route>
-        <Route path={match.path}>
-          <h1 class="center">올바른 경로로 접속해 주세요.</h1>
-          <Link to="/">Home</Link>
-        </Route>
-      </Switch>
-    </div>
-  )
-}
-
-let send
-function Guild() {
-  let { guildId } = useParams()
-  fetch(`http://localhost:3001/api/${guildId}`).then(res=>res.json()).then(data=>send=data)
-  console.log(send)
-  if(!send) return <h1>길드정보가 없습니다. 올바른 경로로 접속했는지 확인해주세요.</h1>
-  let datadata = send.queue.split("\n").join("</tr><th>").split("&yoru%").join('</th><th>')
-  return (
-    <div>
-      <img src={send.thumbnail[0]}></img>
-      <p></p>
-      <h1>현재 재생 중: {send.title[0]}</h1>
-      <Table striped bordered hover>
-
-      </Table>
-    </div>
-  )
 }
 
 export default App;
